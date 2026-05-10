db2e27ff46d08efe1c460194b09b7277c88c1cad
smileman4a <smileman4a@gmail.com>
2026-05-10 22:02:31 +0400

    Домашнее задание по питону. Урок 01

---
+++ lesson_01/lesson_1_task_3.py
@@ -0,0 +1,3 @@
+first_name = input()
+last_name = input()
+print(f'Вас зовут: {first_name} {last_name}')
---
+++ lesson_01/lesson_1_task_5.py
@@ -0,0 +1,5 @@
+def print_num(num):
+    print(num, end='') #o_O нам точно надо было переизобретать принт?
+
+for c in '88005553535':
+    print_num(c)
